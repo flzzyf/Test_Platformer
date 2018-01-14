0a236975047d8c9b4f7d5a044d48657c9ec7979d
Flzzyf <flzzyf076@gmail.com>
2018-01-14 10:42:33 +0800

    动作修改

---
+++ Test_Platformer/Assets/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("qwe");
+        }
+	}
+}

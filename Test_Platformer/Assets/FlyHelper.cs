964b0d46b9bc0c87bd5ee26721262d9c5967ef0a
Flzzyf <flzzyf076@gmail.com>
2018-03-08 18:49:51 +0800

    飞行助手

---
+++ Test_Platformer/Assets/FlyHelper.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyHelper : MonoBehaviour {
+
+    public float wingPowerInit = 3;
+    float wingPower;
+
+    public float wingCooldown = 0.5f;
+    float wingCooldownCurrent;
+
+    public float preferedHeight;
+
+    void Start ()
+    {
+        wingPower = wingPowerInit;
+
+        preferedHeight = transform.position.y;
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WingUp();
+
+        }
+
+        //if(wingCooldownCurrent > 0)
+        //{
+        //    wingCooldownCurrent -= Time.deltaTime;
+        //}
+        //else
+        //{
+        //    wingCooldownCurrent = wingCooldown;
+
+        //    WingUp();
+
+        //}
+
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        preferedHeight += inputV;
+
+        Debug.Log(preferedHeight);
+
+        if(transform.position.y < preferedHeight)
+        {
+            WingUp();
+        }
+    }
+
+    //扇翅膀
+    void WingUp()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        GetComponent<Rigidbody2D>().AddForce(transform.up * wingPower, ForceMode2D.Impulse);
+    }
+}

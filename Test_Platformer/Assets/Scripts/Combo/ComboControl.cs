18aa366885f0d15ea108713dd0f876495c33bd4d
Flzzyf <flzzyf076@gmail.com>
2018-03-13 13:33:44 +0800

    连击系统更改中

--- Test_Platformer/Assets/Scripts/Combo/ComboControl.cs
+++ Test_Platformer/Assets/Scripts/Combo/ComboControl.cs
@@ -22,43 +22,54 @@ public class ComboControl : MonoBehaviour {
             SetComboStatus(i, 0);
         }
 
-
     }
 
     void Update()
     {
         animState = animator.GetCurrentAnimatorStateInfo(0);
 
+        float animPercent = (animState.normalizedTime % animState.length) / animState.length;
+
+        print("动画百分比：" + animPercent);
+
         for (int i = 0; i < combo.Length; i++)
         {
+            if (comboStatusCounter[i] > 0 && animPercent >= 0.9f)
+            {
+                Debug.Log("清零");
+
+                SetComboStatus(i, 0);
+
+            }
+
             if (Input.GetKeyDown(combo[i].action[comboStatusCounter[i]].key))
             {
                 print("key");
 
-                if (comboStatusCounter[i] == 0 || animState.normalizedTime > 0.3f)
+                if (comboStatusCounter[i] == 0 || animPercent > 0.3f)
                 {
                     print("key2");
 
+                    comboStatusCounter[i]++;
+
                     if (comboStatusCounter[i] < combo[0].action.Length) //未完成连击所有动作
                     {
-                        //comboStatusCounter[i]++;
 
                         //print(comboStatusCounter[i]);
 
-                        SetComboStatus(i, ++comboStatusCounter[i]);
+                        SetComboStatus(i, comboStatusCounter[i]);
+                    }
+                    else
+                    {
+                        SetComboStatus(i, 0);
+
                     }
 
                 }
-            }
-
-            if (comboStatusCounter[i] > 0 && animState.normalizedTime >= 0.9f)
-            {
-                Debug.Log("清零");
-
-                //SetComboStatus(i, 0);
-
 
             }
+
+
         }
 
     }
@@ -69,7 +80,7 @@ public class ComboControl : MonoBehaviour {
 
         animator.SetInteger(combo[_index].animStatusName, _status);
 
-        print("" + _index + _status);
+        //print("" + _index + _status);
 
     }
 
